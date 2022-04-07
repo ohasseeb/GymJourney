2996755d1df46d6850b8e1d1469798fa4140eb18
Obaid Hasseeb <hasseeb.obaid.2016@gmail.com>
2022-04-07 13:36:46 -0700

    Added Header Navigation  Styling, Converted Rest Page into Class Component, need to add navigation without reliance on Home, i.e. back buttons on all pages then it's onto the backend

--- src/Screens/HomePage/Rest/RestPage.js
+++ src/Screens/HomePage/Rest/RestPage.js
@@ -10,23 +10,44 @@ import {
 
 import CustomSubmitButton from "../../../CustomSubmitButton/CustomSubmitButton";
 
-const RestPage = () => {
-  return (
-    <View>
-      <Text> Welcome to the Rest Page</Text>
-      <Text>What did you do today?</Text>
-      <TextInput
-        multiline
-        numberOfLines={10}
-        placeholder="Chill, Anime, Code, Read?"
-        style={styles.textInput}
-      />
+class RestPage extends React.Component {
+  constructor(props) {
+    super(props);
+    {
+      this.state = {
+        journalEntry: "",
+      };
+    }
+  } // end ctor
+  render() {
+    const printFunction = () => {
+      console.log("Journal Entry for the Day:", this.state.journalEntry);
+    };
 
-      <CustomSubmitButton RestPage></CustomSubmitButton>
-      {/* <Button title="Submit"> </Button> */}
-    </View>
-  );
-};
+    return (
+      <View>
+        <Text> Welcome to the Rest Page</Text>
+        <Text>What did you do today?</Text>
+        <TextInput
+          multiline
+          numberOfLines={10}
+          placeholder="Chill, Anime, Code, Read?"
+          style={styles.textInput}
+          onChangeText={(textInput) =>
+            this.setState({ journalEntry: textInput })
+          }
+          value={this.state.journalEntry}
+        />
+
+        <CustomSubmitButton
+          RestPage
+          onBtnClick={printFunction}
+        ></CustomSubmitButton>
+        {/* <Button title="Submit"> </Button> */}
+      </View>
+    );
+  }
+} // end Class
 
 const styles = StyleSheet.create({
   textInput: {
